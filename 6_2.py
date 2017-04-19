070b4c5a73f41b242d58a481f0b3d01a3adbab48
Kharmas <kharmas14@gmail.com>
2017-04-19 11:39:17 +0300

    Delete 6_2.py

--- 6_2.py
+++
@@ -1,9 +0,0 @@
-import  re
-
-ex = 'fu, tofu, snafu'
-ex1 = 'futz, fusillade, functional, discombobulated'
-
-pat = re.compile(r'(\w*fu\b)')
-res = pat.findall(ex)
-res1 = pat.findall(ex1)
-print(res,res1)
